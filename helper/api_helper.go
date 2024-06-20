8ae29acbdca01a859145a34385d6e7fef22c8eb9
Dizi <58800976+DiziASP@users.noreply.github.com>
2024-06-20 14:18:01 +0700

    fix: fix stupid bug

--- helper/api_helper.go
+++ helper/api_helper.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 // Helper function to create payload template
 func GenerateResponse(err bool, message string, data any) gin.H {
 	return gin.H{
-		"error":   false,
+		"error":   err,
 		"message": message,
 		"data":    data,
 	}
